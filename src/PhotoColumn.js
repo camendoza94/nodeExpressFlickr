7634a0f2e8d1b9e6aed94d6778a9f9a35cc8470e
camendoza94 <ca.mendoza968@uniandes.edu.co>
2017-03-09 09:07:26 -0500

    Photo column

--- src/PhotoColumn.js
+++ src/PhotoColumn.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import './PhotoColumn.css';
 
-class PhotoColumn extend Component{
+class PhotoColumn extends Component{
 	render(){
-
+		if(this.props.photos) {
+			return(
+				<div className="col-md-2">
+				{this.props.photos.map((photo, index) => {
+						return <Photo photo={photo}/>
+				})};
+				</div>
+				)
+			}
+		}
 	}
 }
 

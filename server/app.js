9ef6225b20435bbeeb16964d67d164feadc0ed26
CAMILO ANDRES MENDOZA ARRIETA <ca.mendoza968@sis.virtual.uniandes.edu.co>
2017-03-10 12:02:43 -0500

    Changed getApiKeys to get from env variables

--- server/app.js
+++ server/app.js
@@ -3,27 +3,20 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const flickr = require("flickrapi");
-const fs = require("fs");
-
 const app = express();
-
-// Assumes that there are two files containing the keys
-// $PROJECT_HOME/server/api_key.txt
-// $PROJECT_HOME/server/api_secret.txt
-function getApiKeys(callback, errorcallback) {
-	fs.readFile(path.resolve(__dirname,"./api_key.txt"), "utf-8", (err, api_key) => {
-		if (err) {
-			errorcallback(err);
-			return;
-		}
-		fs.readFile(path.resolve(__dirname,"./api_secret.txt"), "utf-8",(err, api_secret) => {
-			if (err) {
-				errorcallback(err);
-				return;
-			}
-			callback(api_key, api_secret);
-		});
-	});
+const api_key = process.env.FLICKR_KEY;
+const api_secret = process.env.FLICKR_SECRET;
+// Assumes that there are environmental variables files containing the keys
+// FLICKR_KEY
+// FLICKR_SECRET
+function getApiKeys(callback) {
+	if(api_key && api_secret){
+		callback(api_key, api_secret);
+	}
+	else {
+		console.log("There was an error getting the keys");
+		return;
+	}
 }
 
 // Setup logger
@@ -37,12 +30,12 @@ app.get('/flickr/:query', function (req, res) {
 	console.log("Flickr call query=" + req.params['query'] );
 	getApiKeys((api_key, api_secret) => {
 		const Flickr = require("flickrapi"),
-	    flickrOptions = {
-	      api_key: api_key,
-	      secret: api_secret
-	    };
-	    console.log(api_key);
-	    console.log(api_secret);
+		flickrOptions = {
+			api_key: api_key,
+			secret: api_secret
+		};
+		console.log(api_key);
+		console.log(api_secret);
 		Flickr.tokenOnly(flickrOptions, function(error, flickr) {
 			console.log("tokenOnly");
 			if (error) {
@@ -64,15 +57,12 @@ app.get('/flickr/:query', function (req, res) {
 		  });
 		});
 
-	}, (err) => {
-		console.log(err);
-		res.send("Error!");
 	})
 });
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+	res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 
